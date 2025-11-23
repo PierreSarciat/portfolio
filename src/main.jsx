43fab5a8c3c21915cbc3478badce06a9d3600818
PierreSarciat <phsarciat@mailo.com>
2025-11-23 10:55:03 +0100

    style main/root/body

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client';
-import './index.css'
+/*import './index.css'*/
+import "./main.scss"
 import Router from './router.jsx'; 
 
 
