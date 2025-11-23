1fb6bc732bfae170c7a0c67f5ec316b73cf49580
PierreSarciat <phsarciat@mailo.com>
2025-11-23 17:12:08 +0100

    rajout alias

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './src/components'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@styles': path.resolve(__dirname, './src/styles'),
-     
+       '@images': path.resolve(__dirname, './public/images'),
     },
   },
  
