3ddfbeb2f1207605b8ae672340693d5ba89461e1
PierreSarciat <phsarciat@mailo.com>
2025-11-23 10:53:25 +0100

    mise en place du header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,47 @@
 import React from "react";
 import './header.scss'
+import { NavLink } from "react-router-dom";
 
 
 const Header = () => {
   return (
     <header>
+      <NavLink to="" >
+        Pierre-Henri Sarciat
+      </NavLink>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="">
+              Présentation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="">
+              Compétences
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="">
+              Portfolio
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="">
+              A propos
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
 
-      
-     
-          <p> Header </p>   
-          
+
+
+
     </header>
   );
 };
