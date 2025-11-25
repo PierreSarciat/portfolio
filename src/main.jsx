c8a9a69eb29b16f3ca9d0166073ffb876c8a240e
PierreSarciat <phsarciat@mailo.com>
2025-11-25 14:00:35 +0100

    maj

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client';
-/*import './index.css'*/
 import "./main.scss"
-import Router from './router.jsx'; 
+import Router from './router.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <Router />
+    <Router />
   </StrictMode>,
 )
