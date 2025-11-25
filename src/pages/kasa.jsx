e95aa76d05b24d979f87f283bfba76714c1f03db
PierreSarciat <phsarciat@mailo.com>
2025-11-25 16:19:00 +0100

    création page kasa

--- src/pages/kasa.jsx
+++ src/pages/kasa.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import Button from "@components/button.jsx";
+import "./kasa.scss";
+import logoGitHub from "@images/logoGitHub.png";
+
 
 const Kasa = () => {
     return (
         <div>
-            <h1>Projet kasa</h1>
-            <p>Description détaillée du projet kasa..</p>
+            <section className='titleProject'>
+                <h1>Kasa</h1>
+                <p>Créer une application de location immobilière avec React</p>
+            </section>
+
+            <section className='mockupContent'>
+                <div className='mockup'>
+                    <img src="/portfolio/mockup/kasa/kasa1.png" alt="Mockup Kasa" />
+                    <img src="/portfolio/mockup/kasa/kasa2.png" alt="Mockup Kasa" />
+                    <img src="/portfolio/mockup/kasa/kasa3.png" alt="Mockup Kasa" />
+                    <img src="/portfolio/mockup/kasa/kasa4.png" alt="Mockup Kasa" />
+                    <img src="/portfolio/mockup/kasa/kasa5.png" alt="Mockup Kasa" />
+                </div>
+                <div className='mockupProject'>
+                    <h2>Projet: </h2>
+                    <ul>
+                        <li>Interface React</li>
+                        <li>Navigation React Router</li>
+                        <li>Style Sass</li>
+                    </ul>
+                </div>
+
+            </section>
+            <div className='buttons'>
+                <Button img={logoGitHub} alt="GitHub" href="https://github.com/PierreSarciat/Kasa-immobilier.git" target="_blank" rel="noopener noreferrer" />
+                <Button text="Mon CV" href="/cv.pdf" />
+            </div>
         </div>
     );
 };
