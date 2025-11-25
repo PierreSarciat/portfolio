6bfcaacd322862ff0806aa886e1935614196f3f1
PierreSarciat <phsarciat@mailo.com>
2025-11-25 15:55:21 +0100

    création page carducci

--- src/pages/carducci.jsx
+++ src/pages/carducci.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import Button from "@components/button.jsx";
+import "./carducci.scss";
+import logoGitHub from "@images/logoGitHub.png";
 
-const Carducci = () => {
+
+const Booki = () => {
     return (
         <div>
-            <h1>Projet Nina Carducci</h1>
-            <p>Description détaillée du projet Nina Carducci...</p>
+            <section className='titleProject'>
+                <h1>Nina Carducci</h1>
+                <p>Débugger et optimiser un site de photographe</p>
+            </section>
+
+            <section className='mockupContent'>
+                <div className='mockup'>
+                    <img src="/portfolio/mockup/carducci/carducci1.png" alt="Mockup Carducci" />
+                    <img src="/portfolio/mockup/carducci/carducci2.png" alt="Mockup Carducci" />
+                    <img src="/portfolio/mockup/carducci/carducci3.png" alt="Mockup Carducci" />
+                </div>
+                <div className='mockupProject'>
+                    <h2>Projet: </h2>
+                    <ul>
+                        <li>Rapport d' audit SEO</li>
+                        <li>Accessibilité</li>
+                        <li>Cahier de recette avec tests</li>
+                    </ul>
+                </div>
+
+            </section>
+            <div className='buttons'>
+                <Button img={logoGitHub} alt="GitHub" href="https://github.com/PierreSarciat/ninacarducci.git" target="_blank" rel="noopener noreferrer" />
+                <Button text="Rapport d' optimisation" href="public/mockup/carducci/auditSEO.pdf" target="_blank" rel="noopener noreferrer" />
+            </div>
         </div>
     );
 };
 
-export default Carducci;
+export default Booki;
