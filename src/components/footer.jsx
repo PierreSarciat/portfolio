8ab81116effcb7215110f9e4ee652ca2efa322f7
PierreSarciat <phsarciat@mailo.com>
2025-11-29 11:08:11 +0100

    maj contact

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Facebook from '@assets/logo/facebook.png';
 import Instagram from '@assets/logo/instagram.jpg';
-import Twitter from '@assets/logo/twitter.jpg';
+import MAIL from '@assets/logo/logoMail.JPG';
+import LKDN from '@assets/logo/logoLDN.png';
 import './footer.scss';
 
 const Footer = () => {
@@ -11,14 +12,13 @@ const Footer = () => {
       <nav>
         <ul>
           <li>
-            <NavLink to="" id="contact"> <img src={Facebook} alt="logo facebook" className="footer-logo" /></NavLink>
+            <NavLink to="mailto:phsarciat@mailo.com" id="contact" target="_blank" rel="noopener noreferrer"> <img src={MAIL} alt="logo email" className="footer-logo" />phsarciat@mailo.com</NavLink>
           </li>
           <li>
-            <NavLink to=""> <img src={Instagram} alt="logo instagram" className="footer-logo" /></NavLink>
+            <NavLink to="https://www.linkedin.com/in/pierre-henri-sarciat-55728219b" target="_blank" rel="noopener noreferrer"> <img src={LKDN} alt="logo linkdin" className="footer-logo" /></NavLink>
           </li>
-          <li>
-            <NavLink to=""> <img src={Twitter} alt="logo twitter" className="footer-logo" /></NavLink>
-          </li>
+
+
         </ul>
       </nav>
     </div>
